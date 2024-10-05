cbd6f45ded8e57ba848a01b5a88a8068a5b50fc2
momo-41 <minimizu41@gmail.com>
2024-10-05 14:59:12 +0900

    Promiseと非同期処理を使って、3秒後にフルネームを表示する関数を作成

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,33 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await Promise.all(userList.map(test));
+  return await result;
 }
 
-function test() {
-  return
-}
+// function test(data) {
+//   return { ...data, full_name: `${data.family_name} ${data.first_name}` };
+// }
 
+function test(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ ...data, full_name: `${data.family_name} ${data.first_name}` });
+    }, 3000); // 3秒遅延
+  });
+}
